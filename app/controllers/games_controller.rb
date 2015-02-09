a23dc06c2ae0c19951fc85a34f0e6dbe749bd4b9
Rob Zimmerman <rzimmerman@dynamicquest.com>
2015-02-09 16:04:27 -0500

    Efficient Vote Sorting on Games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,33 +1,34 @@
 class GamesController < ApplicationController
-  
+
   def index
     @games = Game.all.sort_by {|game| game.votes.count}.reverse!
   end
-  
+
   def show
-    @game = Game.find(params[:id])
-    @current_user_vote = Vote.where(game: @game, user: current_user).first if current_user
-    
+    @game = Game.includes(:votes, :time_slot).find(params[:id])
+    @current_user_vote = @game.votes.where(user: current_user).first if current_user
+
     @available_times = TimeSlot.not.in(id: Game.where(:id.ne => @game.id).pluck(:time_slot_id)).sort_by { |time_slot| time_slot.scheduled_time } # List of every time slot that isn't currently filled
-    
+
     # List of players signed up for the game
     @players = {}
     # Score submissions (use cl_image_tag to display the value for each user as a key)
     @scores = {}
-    
+
+    # Build the teams for sorting players into
     Team.each do |team|
       @players[team.name] = []
     end
-    
+
     @game.votes.each do |vote|
       @players[vote.user.team.name] << vote.user
       @scores[vote.user.username] = vote.image_id
     end
   end
-  
+
   def update
     @game = Game.find(params[:id])
-    
+
     if @game.update_attributes(params[:game]) && @game.save
       render(json: {type: 'game', action: 'set_time_slot', game: @game, success: true, params: params})
     else
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Taggames2014::Application.config.session_store :cookie_store, key: '_taggames2014_session'
+TagGames::Application.config.session_store :cookie_store, key: '_taggames_session'
