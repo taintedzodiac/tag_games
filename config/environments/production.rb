a2266d425ed6f576ba2897bbbedf8ae20bf4b1e6
Rob Zimmerman <rzimmerman@dynamicquest.com>
2015-02-09 16:29:11 -0500

    Set host for ActionMailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ TagGames::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Host for production
+  config.action_mailer.default_url_options = { :host => "taggames2014.herokuapp.com" }
 end
