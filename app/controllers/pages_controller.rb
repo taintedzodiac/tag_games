d33ebd428beebf06a45fb1f107bf9178e747102d
Rob Zimmerman <rob.zimmerman@cyanna.com>
2015-04-27 09:31:00 -0400

    Fix for game platform names

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 class PagesController < ApplicationController
-  
+
   def home
-    @xbox_one_games = Game.where(platform: "Microsoft Xbox One").asc(:title)
-    @ps4_games = Game.where(platform: "Sony Playstation 4").asc(:title)
-    @xbox_360_games = Game.where(platform: "Microsoft Xbox 360").asc(:title)
-    @other_games = Game.where(platform: "Other").asc(:title)
-    
+    @xbox_one_games = Game.where(platform: "Xbox One").asc(:title)
+    @ps4_games = Game.where(platform: "Playstation 4").asc(:title)
+    @xbox_360_games = Game.where(platform: "Xbox 360").asc(:title)
+    @other_games = Game.where(platform: "PC").asc(:title)
+
     @user_votes = current_user.votes.with(:game) if user_signed_in?
   end
 end
