dbfd5ce7718a1dd74c741f097277c860bf04c66b
Rob Zimmerman <rob.zimmerman@cyanna.com>
2015-04-29 14:01:52 -0400

    Wii U listings

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
     @xbox_one_games = Game.where(platform: "Xbox One").asc(:title)
     @ps4_games = Game.where(platform: "PS4").asc(:title)
     @xbox_360_games = Game.where(platform: "Xbox 360").asc(:title)
+    @wii_u_games = Game.where(platform: "Wii U").asc(:title)
     @other_games = Game.where(platform: "PC").asc(:title)
 
     @user_votes = current_user.votes.with(:game) if user_signed_in?
