6a2e1e87b39b83d2b6e848e05d11c6f778bda49a
Rob Zimmerman <rob.zimmerman@cyanna.com>
2015-04-29 10:10:28 -0400

    Team assignment fixes

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,11 +1,11 @@
 class Team
   include Mongoid::Document
-  
+
   field :name,        type: String,     default: ''
   field :logo,        type: String,     default: ''
-  
+
   has_many :members, class_name: 'User', inverse_of: :team # Players on this team
   has_one :captain, class_name: 'User', inverse_of: :captain_team # Each team has a captain
   has_many :assistants, class_name: 'User', inverse_of: :assistant_team # Each team can have assistant captains
-  
+
 end
