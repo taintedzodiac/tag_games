cc2dfa1f580cbea3f970e6f0394f7eee7864acea
Rob Zimmerman <rob.zimmerman@cyanna.com>
2015-05-06 13:11:52 -0400

    Ongoing listings in schedule

--- app/controllers/time_slots_controller.rb
+++ app/controllers/time_slots_controller.rb
@@ -1,7 +1,8 @@
 class TimeSlotsController < ApplicationController
-  
+
   def index
+    @ongoing_events = Game.ongoing
     @time_slots = TimeSlot.asc(:scheduled_time)
   end
-  
+
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,19 +1,21 @@
 class Game
   include Mongoid::Document
-  
+
   field :title,       type: String,     default: ''
   field :platform,    type: String,     default: ''
   field :boxart,      type: String,     default: 'http://farm4.staticflickr.com/3720/13107469424_081ffabd6f.jpg'
   field :tag_thread,  type: String,     default: ''
   field :ongoing,     type: Boolean,    default: false
-  
+
   has_many :votes, dependent: :delete # Users can vote on which games should be included
   belongs_to :time_slot
-  
+
+  scope :ongoing, ->{ where(ongoing: true) }
+
   def title_with_platform
     return "#{self.title} (#{self.platform})"
   end
-  
+
   def dummy
     # This is just a dummy method for autocomplete
   end
